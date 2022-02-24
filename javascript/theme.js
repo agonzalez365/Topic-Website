584588a5813d6e23e720c96de999b49a8d025bd6
Christian <christianlockley4@gmail.com>
2022-02-24 15:41:57 -0800

    linked up and finished the theme change script

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,11 +1,18 @@
-let theme = () => {
-    let themes = ['dark', 'light', 'navy'];
-    let theme = themes[i];
-    let selector = () => {
-        if (theme = clicked) {
-            let [i] = clicked;
-            theme = themes[i];
-        }
-    }
-    return theme;
-}
+$(document).ready(function() {
+    const hex = ['1B4965', 'C9D2D1', '23303D'];
+
+    let inputs = $('#profile').children('button');
+
+    for (let i = 0; i < inputs.length; i += 1) {
+        inputs.eq(i).on('click', function() {
+            $('body').css('--nav-main-color', `#${hex[i]}`);
+        });
+        console.log('yo')
+    } 
+});
+
+/*          
+            <button id="navy" class="btn">Navy</button>
+            <button id="light" class="btn">Light</button>
+            <button id="dark" class="btn">Dark</button>
+*/
