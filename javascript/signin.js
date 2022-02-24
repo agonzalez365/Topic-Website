ef02926e9aa3ea71d66874df4e2f8f17d47be686
agonzalez365 <drewgonz974@gmail.com>
2022-02-24 14:50:35 -0800

    Sign-in now checks for incorrect username

--- javascript/signin.js
+++ javascript/signin.js
@@ -5,6 +5,7 @@ $('document').ready(function() {
         event.preventDefault();
 
         const childInputs = $(this).children('input');
+        const username = childInputs.eq(0).val()
         const password = childInputs.eq(1).val();
         let user = JSON.parse(window.localStorage.getItem('user'));
         if(user === null){
@@ -12,10 +13,12 @@ $('document').ready(function() {
         } else {
             if(user.password !== password){
                 alert('Invalid username or password.');
+            } else if (user.username !== username){
+                alert('Invalid username or password.');
             } else {
                 localStorage.setItem('user', JSON.stringify(user));
                 localStorage.setItem('logged-in', true);
-                window.location.href = 'index.html'
+                window.location.href = 'index.html';
             }
         }
 
