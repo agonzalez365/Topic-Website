f7d78945713764e2ec8ea4458b9a08d0b9edd9bd
Christian <christianlockley4@gmail.com>
2022-02-23 16:26:40 -0800

    added theme.js file

---
+++ javascript/theme.js
@@ -0,0 +1,11 @@
+let theme = () => {
+    let themes = ['dark', 'light', 'navy'];
+    let theme = themes[i];
+    let selector = () => {
+        if (theme = clicked) {
+            let [i] = clicked;
+            theme = themes[i];
+        }
+    }
+    return theme;
+}
