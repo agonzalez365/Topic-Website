cbee46f3216235eeba25fc90d6c7d259098929e3
Christian <christianlockley4@gmail.com>
2022-03-07 14:57:20 -0800

    experimenting with variable js

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     const hex = ['1B4965', 'C9D2D1', '23303D'];
 
-    let inputs = $('#profile').children('button');
+    let inputs = $('ul').children('button');
 
     for (let i = 0; i < inputs.length; i += 1) {
         inputs.eq(i).on('click', function() {
