c7092f2a10e04e7a92403a3e0c852b66f0ccf615
agonzalez365 <drewgonz974@gmail.com>
2022-03-22 16:26:01 -0700

    Even more fight sign in html update logic

--- javascript/global.js
+++ javascript/global.js
@@ -1,15 +1,17 @@
 $('document').ready(function() {
     //check login status, update html accordingly
     const signInBtn = document.getElementById('sign-in-btn');
-    // if(localStorage.getItem('logged-in') === "true"){
-    //     signInBtn.innerHTML = `Sign out <i class="fas fa-user"></i>`;
-    // }
+
+    //JS Media Queries
+    medQuery = window.matchMedia('(max-width: 1000px)');
 
     //sign user out if logged in, else bring user to log in page
     signInBtn.addEventListener('click', function(event) {
         if(localStorage.getItem('logged-in') === "true") {
             event.preventDefault();
-            signInBtn.innerHTML = `Sign in <i class="fas fa-user"></i>`;
+            if(!query.matches){
+                signInBtn.innerHTML = `Sign in <i class="fas fa-user"></i>`;
+            }
             localStorage.setItem('logged-in', false);
         }
     });
@@ -18,8 +20,7 @@ $('document').ready(function() {
          $('#nav-links').toggleClass('hidden');
     });
 
-    //JS Media Queries
-    medQuery = window.matchMedia('(max-width: 1000px)');
+    
 
     //updates html on page load
     if(medQuery.matches){
@@ -38,7 +39,12 @@ $('document').ready(function() {
             $('#profile').children().eq(0).html('<i class="fas fa-user">');;
         }
         else {
-            $('#profile').children().eq(0).html('Sign in <i class="fas fa-user">');
+            if(localStorage.getItem('logged-in') === "true") {
+                $('#profile').children().eq(0).html('Sign out <i class="fas fa-user">')
+            } else {
+                $('#profile').children().eq(0).html('Sign in <i class="fas fa-user">');
+            }
+            
         }
         
     });
